2d4f0d8360d97073e13edbbcb6c1257e5ffa7957
Ojas Shukla <54703305+whizzbbig@users.noreply.github.com>
2023-11-23 22:23:26 +0530

    UPDATES

---
+++ src/pages/_app.js
@@ -0,0 +1,63 @@
+import 'styles/index.scss'
+
+import Head from 'next/head'
+import Lenis from 'utils/scroll'
+import Navigation from 'sections/shared/Navigation'
+import Loader from 'components/Loader'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+import Link from 'next/link'
+import GSAP from 'gsap'
+import Main from 'components/Main'
+import { PrismicProvider } from '@prismicio/react'
+import { PrismicPreview } from '@prismicio/next'
+import { repositoryName } from '../../prismicio'
+
+const App = ({ Component, pageProps }) => {
+  const { events } = useRouter()
+
+  Lenis
+
+  useEffect(() => {
+    events.on('routeChangeComplete', () => {
+      GSAP.to('main', { autoAlpha: 1, duration: 0.6 })
+      window.scroll(0, 0)
+    })
+  }, [])
+
+  return (
+    <PrismicProvider internalLinkComponent={(props) => <Link {...props} />}>
+      <PrismicPreview repositoryName={repositoryName}>
+        <Head>
+          <title>Studio Oriell</title>
+          <meta name="description" content="Studio Oriell" />
+          <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+          <link rel="apple-touch-icon" sizes="57x57" href="/imgs/meta/apple-icon-57x57.png" />
+          <link rel="apple-touch-icon" sizes="60x60" href="/imgs/meta/apple-icon-60x60.png" />
+          <link rel="apple-touch-icon" sizes="72x72" href="/imgs/meta/apple-icon-72x72.png" />
+          <link rel="apple-touch-icon" sizes="76x76" href="/imgs/meta/apple-icon-76x76.png" />
+          <link rel="apple-touch-icon" sizes="114x114" href="/imgs/meta/apple-icon-114x114.png" />
+          <link rel="apple-touch-icon" sizes="120x120" href="/imgs/meta/apple-icon-120x120.png" />
+          <link rel="apple-touch-icon" sizes="144x144" href="/imgs/meta/apple-icon-144x144.png" />
+          <link rel="apple-touch-icon" sizes="152x152" href="/imgs/meta/apple-icon-152x152.png" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/imgs/meta/apple-icon-180x180.png" />
+          <link rel="icon" type="image/png" sizes="192x192" href="/imgs/meta/android-icon-192x192.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/imgs/meta/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="96x96" href="/imgs/meta/favicon-96x96.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/imgs/meta/favicon-16x16.png" />
+          <link rel="manifest" href="/imgs/meta/manifest.json" />
+          <meta name="msapplication-TileColor" content="#ffffff" />
+          <meta name="msapplication-TileImage" content="/imgs/meta/ms-icon-144x144.png" />
+          <meta name="theme-color" content="#ffffff" />
+        </Head>
+        <Navigation />
+        <Loader/>
+        <Main>
+          <Component {...pageProps} />
+        </Main>
+      </PrismicPreview>
+    </PrismicProvider>
+  );
+};
+
+export default App;
